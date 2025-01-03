6232dea007cb097a9692c03d63b474515d021aa1
varshikaveeramalla <111838330+varshikaveeramalla@users.noreply.github.com>
2025-01-03 12:07:26 +0530

    Create helloworld.java

---
+++ helloworld.java
@@ -0,0 +1,7 @@
+// HelloWorld.java
+public class HelloWorld {
+    public static void main(String[] args) {
+        // Output "Hello, World!" to the console
+        System.out.println("Hello, World!");
+    }
+}
